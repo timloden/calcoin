7fd7bc9a6bcfba232bb58b8bcc3e08f26a1f1624
Tim Loden <tim.m.loden@gmail.com>
2019-05-07 14:37:29 -0700

    added multiste check for migrating logo

--- inc/menus.php
+++ inc/menus.php
@@ -1,5 +1,10 @@
 <?php
-// Register your menus
+
+/* Register menus
+--------------------------------------------------------------------------------------*/
+
+add_action( 'init', 'my_custom_menus' );
+
 function my_custom_menus() {
     $locations = array(
     	'header-menu' => esc_html__( 'Header Menu', 'caweb-standard' ),
@@ -7,22 +12,3 @@ function my_custom_menus() {
     );
     register_nav_menus( $locations );
  }
-
-// Hook them into the theme-'init' action
-add_action( 'init', 'my_custom_menus' );
-
-
-function find_multisites() {
-	if ( is_multisite() ) {
-		$subsites = get_sites();
-		foreach( $subsites as $subsite ) {
-		  $subsite_id = get_object_vars($subsite)["blog_id"];
-		  $subsite_name = get_blog_details($subsite_id)->blogname;
-		  echo 'Site ID/Name: ' . $subsite_id . ' / ' . $subsite_name . '\n';
-		}
-	} else {
-		echo 'not multisite';
-	}
-}
-
-add_action( 'init', 'find_multisites' );

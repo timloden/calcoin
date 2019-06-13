92835d72be47b1e79887892fc758047367a3d0fc
Tim Loden <tim.m.loden@gmail.com>
2019-06-13 14:26:10 -0700

    added temp links on the home page

--- front-page.php
+++ front-page.php
@@ -8,14 +8,16 @@ get_header();
 
 ?>
 
-<div class="row">
-	<div class="columns">
-		<h1>Home</h1>
+<div class="row align-center">
+	<div class="columns small-10">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/login-logo.png">
 	</div>
 </div>
 <div class="row">
-	<div class="columns">
-
+	<div class="columns text-center" style="margin-top: 2em;">
+		<a href="<?php echo site_url(); ?>/dashboard" class="button button-primary">Dashboard</a>
+		<a href="<?php echo site_url(); ?>/login" class="button button-primary">Login</a>
+		<a href="<?php echo site_url(); ?>/signup" class="button button-primary">Signup</a>
 	</div>
 </div>
 

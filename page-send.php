848dfe289aeb66510d368ddf8908e9d5b9792ba5
Tim Loden <tim.m.loden@gmail.com>
2019-06-17 18:18:46 -0700

    removed other qr code libraries and added the working one

--- page-send.php
+++ page-send.php
@@ -9,6 +9,43 @@
 
 get_header('app');
 ?>
+<!--
+<div>
+    <b>Device has camera: </b>
+    <span id="cam-has-camera"></span>
+    <br>
+    <video muted playsinline id="qr-video" style="width: 300px;"></video>
+</div>
+-->
+
+<script type="module">
+    import QrScanner from "/wp-content/themes/calcoin/assets/js/vendor/lib/qr-scanner.min.js";
+    QrScanner.WORKER_PATH = '/wp-content/themes/calcoin/assets/js/vendor/lib/qr-scanner-worker.min.js';
+    
+    const video = document.getElementById('qr-video');
+    const camQrResult = document.getElementById('cam-qr-result');
+
+    function setResult(label, result) {
+        label.value = result;
+    }
+
+    const scanner = new QrScanner(video, result => setResult(camQrResult, result));
+    
+    let camera = document.getElementById('scanner');
+        
+    document.getElementById('open-camera').addEventListener('click', ()=> {
+	    
+	    camera.classList.add('animated', 'fadeIn', 'faster');
+	    scanner.start();
+	});
+	
+	document.getElementById('close-camera').addEventListener('click', ()=> {
+	    
+	    camera.classList.remove('animated', 'fadeIn', 'faster');
+	    
+	});
+</script>
+
 <div class="row">
 	<div class="columns">
 		<div class="card">
@@ -20,18 +57,18 @@ get_header('app');
 					100000
 				</div>
 			</div>
-			<div class="scanner">
+			<div id="scanner" class="scanner">
 				<p>
-					<a class="close-camera"><i class="la la-close"></i></a>
+					<a id="close-camera" class="close-camera"><i class="la la-close"></i></a>
 				</p>
-				<div id="reader" style="width:300px;height:250px"></div>
+				<video muted playsinline id="qr-video" style="width: 100%;"></video>
 			</div>
 			<div class="send-form">
 				<form>
 					<label>Send to:</label>
 					<div class="send-to-field">
-						<input class="send-address" type="text" placeholder="0x4B4D...">
-						<a class="button small open-camera"><i class="la la-camera"></i></a>
+						<input id="cam-qr-result" class="send-address" type="text" placeholder="0x4B4D...">
+						<a id="open-camera" class="button small open-camera"><i class="la la-camera"></i></a>
 					</div>
 
 					<div class="send-amount">
@@ -50,29 +87,21 @@ get_header('app');
 </div>
 
 <script>
+/*
 (function ($) {
 	$( '.open-camera' ).click(function() {
 		$('.scanner').toggle().addClass('animated fadeIn faster');
 	});
 
 	$( '.close-camera' ).click(function() {
-		$('.scanner').toggle();
+		$('.scanner').hide();
+		$('#reader').html5_qrcode_stop();
 	});
 
-	$('#reader').html5_qrcode(function(data){
-	 		// do something when code is read
-	 		console.log('code scanned');
-	 		$('.send-address').val(data);
-	 	},
-	 	function(error){
-			//show read errors
-		}, function(videoError){
-			//the video stream could be opened
-		}
-	);
 
 
 })(jQuery);
+*/
 </script>
 
  <?php
